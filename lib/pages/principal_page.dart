f07fc3df413f47ec04c495d16c0eeedceee27fb9
yamilsaad <yamilsaad00@gmail.com>
2023-10-17 19:33:36 -0300

    correccion de padding

--- lib/pages/principal_page.dart
+++ lib/pages/principal_page.dart
@@ -20,95 +20,118 @@ class PrincipalPage extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: Stack(children: [
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 5.0).dg,
-                child: Stack(children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5.0).dg,
-                        child: FlipCard(
-                          fill: Fill.fillFront,
-                          direction: FlipDirection.VERTICAL,
-                          side: CardSide.FRONT,
-                          front: Container(
-                            margin: const EdgeInsets.only(top: 0).dg,
-                            height: 100.h,
-                            width: 70.w,
-                            constraints: BoxConstraints(maxHeight: 40.dg),
-                            decoration: BoxDecoration(
-                                color: const Color(0xFF053B50),
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Padding(
-                              padding: const EdgeInsets.all(0.5).dg,
-                              child: Center(
-                                child: Text(
-                                  'Soy.Dev',
-                                  style: GoogleFonts.roboto(
-                                      fontSize: 16.sp,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 2.0).dg,
+                  child: Stack(children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5.0).dg,
+                          child: FlipCard(
+                            fill: Fill.fillFront,
+                            direction: FlipDirection.VERTICAL,
+                            side: CardSide.FRONT,
+                            front: Container(
+                              margin: const EdgeInsets.only(top: 0).dg,
+                              height: 100.h,
+                              width: 70.w,
+                              constraints: BoxConstraints(maxHeight: 40.dg),
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF053B50),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(0.5).dg,
+                                child: Center(
+                                  child: Text(
+                                    'Soy.Dev',
+                                    style: GoogleFonts.roboto(
+                                        fontSize: 16.sp,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            back: Container(
+                              margin: const EdgeInsets.only(top: 0).dg,
+                              height: 100.h,
+                              width: 70.w,
+                              constraints: BoxConstraints(maxHeight: 35.dg),
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF053B50),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(0.5).dg,
+                                child: Column(
+                                  children: [
+                                    Center(
+                                      child: Text(
+                                        'Soy.Dev ❤️',
+                                        style: GoogleFonts.roboto(
+                                            fontSize: 6.sp,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.red),
+                                      ),
+                                    ),
+                                    Divider(
+                                      height: 2.h,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(1.0).dg,
+                                      child: Center(
+                                        child: Text(
+                                          '<Progr@mador> /Des@rrollador de Software/ #Diseñador# 🎨📱❤️',
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 4.sp,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    )
+                                  ],
                                 ),
                               ),
                             ),
                           ),
-                          back: Container(
-                            margin: const EdgeInsets.only(top: 0).dg,
-                            height: 100.h,
-                            width: 70.w,
-                            constraints: BoxConstraints(maxHeight: 35.dg),
-                            decoration: BoxDecoration(
-                                color: const Color(0xFF053B50),
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Padding(
-                              padding: const EdgeInsets.all(0.5).dg,
-                              child: Column(
-                                children: [
-                                  Center(
-                                    child: Text(
-                                      'Soy.Dev ❤️',
-                                      style: GoogleFonts.roboto(
-                                          fontSize: 6.sp,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.red),
-                                    ),
-                                  ),
-                                  Divider(
-                                    height: 2.h,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(1.0).dg,
-                                    child: Center(
-                                      child: Text(
-                                        '<Progr@mador> /Des@rrollador de Software/ #Diseñador# 🎨📱❤️',
-                                        style: GoogleFonts.roboto(
-                                            fontSize: 4.sp,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ),
-                                  )
-                                ],
+                        ),
+                        SizedBox(
+                          width: 130.w,
+                        ),
+
+                        /*Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: 8.0).dg,
+                              child: MouseRegion(
+                                cursor: SystemMouseCursors.click,
+                                child: Container(
+                                  height: 40.h,
+                                  width: 40.w,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white12,
+                                      borderRadius: BorderRadius.circular(25)),
+                                  child: TextButton(
+                                      onPressed: () {},
+                                      child: Center(
+                                        child: Text(
+                                          'Me',
+                                          style: GoogleFonts.raleway(
+                                              fontSize: 7.sp,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      )),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 130.w,
-                      ),
-
-                      /*Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8.0).dg,
-                            child: MouseRegion(
-                              cursor: SystemMouseCursors.click,
+                            Padding(
+                              padding: const EdgeInsets.only(right: 8.0).dg,
                               child: Container(
                                 height: 40.h,
                                 width: 40.w,
@@ -118,90 +141,70 @@ class PrincipalPage extends StatelessWidget {
                                 child: TextButton(
                                     onPressed: () {},
                                     child: Center(
-                                      child: Text(
-                                        'Me',
-                                        style: GoogleFonts.raleway(
-                                            fontSize: 7.sp,
-                                            fontWeight: FontWeight.bold),
-                                      ),
+                                      child: Text('My Profile',
+                                          style: GoogleFonts.raleway(
+                                              fontSize: 7.sp,
+                                              fontWeight: FontWeight.bold)),
                                     )),
                               ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8.0).dg,
-                            child: Container(
-                              height: 40.h,
-                              width: 40.w,
-                              decoration: BoxDecoration(
-                                  color: Colors.white12,
-                                  borderRadius: BorderRadius.circular(25)),
-                              child: TextButton(
-                                  onPressed: () {},
-                                  child: Center(
-                                    child: Text('My Profile',
-                                        style: GoogleFonts.raleway(
-                                            fontSize: 7.sp,
-                                            fontWeight: FontWeight.bold)),
-                                  )),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 8.0),
+                              child: Container(
+                                height: 40.h,
+                                width: 40.w,
+                                decoration: BoxDecoration(
+                                    color: Colors.white12,
+                                    borderRadius: BorderRadius.circular(25)),
+                                child: TextButton(
+                                    onPressed: () {},
+                                    child: Center(
+                                      child: Text('Contant',
+                                          style: GoogleFonts.raleway(
+                                              fontSize: 7.sp,
+                                              fontWeight: FontWeight.bold)),
+                                    )),
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8.0),
-                            child: Container(
-                              height: 40.h,
-                              width: 40.w,
-                              decoration: BoxDecoration(
-                                  color: Colors.white12,
-                                  borderRadius: BorderRadius.circular(25)),
-                              child: TextButton(
-                                  onPressed: () {},
-                                  child: Center(
-                                    child: Text('Contant',
-                                        style: GoogleFonts.raleway(
-                                            fontSize: 7.sp,
-                                            fontWeight: FontWeight.bold)),
-                                  )),
-                            ),
-                          ),
-                        ],
-                      ),*/
+                          ],
+                        ),*/
+                      ],
+                    ),
+                  ]),
+                ),
+                SizedBox(
+                  height: 50.h,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0).dg,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //CuboWidget(),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      Stack(children: [
+                        const SoydevWidget(),
+                        Container(
+                            width: 170.w,
+                            height: 400.h,
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                //TarjetaTresdeWidget(),
+                              ],
+                            )),
+                      ]),
+                      SizedBox(
+                        width: 1.w,
+                      ),
                     ],
                   ),
-                ]),
-              ),
-              SizedBox(
-                height: 50.h,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10.0).dg,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //CuboWidget(),
-                    SizedBox(
-                      width: 10.w,
-                    ),
-                    Stack(children: [
-                      const SoydevWidget(),
-                      Container(
-                          width: 170.w,
-                          height: 400.h,
-                          child: const Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              //TarjetaTresdeWidget(),
-                            ],
-                          )),
-                    ]),
-                    SizedBox(
-                      width: 1.w,
-                    ),
-                  ],
                 ),
-              ),
-              const ButtonsComponent(),
-            ],
+                const ButtonsComponent(),
+              ],
+            ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
